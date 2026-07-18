078b31367b63d946b3cf3f3f0ef11e20b19eb99a
Abdur Rahman <prince.ar.abdur.rahman2008@gmail.com>
2026-07-18 19:28:28 +0600

    Change applicationId in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.scienceverse.fghjkl"
+    applicationId = "nexvora.scienceverse.rahman"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
